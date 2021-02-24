711728b1da75a8fb6680c9c1d47932269e2842f6
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-02-24 11:05:42 +0100

    Add business services test full form

--- cypress/integration/newBusinessService.test.js
+++ cypress/integration/newBusinessService.test.js
@@ -8,7 +8,7 @@ context("Test NewBusinessService", () => {
     cy.get("@tokens").then((tokens) => {
       const headers = {
         "Content-Type": "application/json",
-        "Accept": "application/hal+json",
+        Accept: "application/hal+json",
         Authorization: "Bearer " + tokens.access_token,
       };
 
@@ -17,15 +17,51 @@ context("Test NewBusinessService", () => {
         method: "GET",
         headers: headers,
         url: `${Cypress.env("controls_base_url")}/business-service?size=1000`,
-      }).then((result) => {
-        result.body._embedded["business-service"].forEach((e) => {
-          cy.request({
-            method: "DELETE",
-            headers: headers,
-            url: `${Cypress.env("controls_base_url")}/business-service/${e.id}`,
+      })
+        .then((result) => {
+          result.body._embedded["business-service"].forEach((e) => {
+            cy.request({
+              method: "DELETE",
+              headers: headers,
+              url: `${Cypress.env("controls_base_url")}/business-service/${
+                e.id
+              }`,
+            });
           });
+        })
+
+        // Delete all stakeholders
+        .then(() => {
+          cy.request({
+            method: "GET",
+            headers: headers,
+            url: `${Cypress.env("controls_base_url")}/stakeholder?size=1000`,
+          });
+        })
+        .then((response) => {
+          response.body._embedded["stakeholder"].forEach((elem) => {
+            cy.request({
+              method: "DELETE",
+              headers: headers,
+              url: `${Cypress.env("controls_base_url")}/stakeholder/${elem.id}`,
+            });
+          });
+        })
+
+        // Create stakeholders
+        .then(() => {
+          for (let i = 1; i <= 12; i++) {
+            cy.request({
+              method: "POST",
+              headers: headers,
+              body: {
+                name: `any`,
+                displayName: `stakeholder${i}`,
+              },
+              url: `${Cypress.env("controls_base_url")}/stakeholder`,
+            });
+          }
         });
-      });
     });
   });
 
@@ -55,5 +91,47 @@ context("Test NewBusinessService", () => {
     cy.get("tbody > tr")
       .should("have.length", 1)
       .contains("my business service");
+  });
+
+  it("Fill all fields", () => {
+    cy.intercept({
+      method: "GET",
+      url: "/api/controls/business-service",
+    }).as("apiCheckBusinessService");
+
+    cy.intercept({
+      method: "GET",
+      url: "/api/controls/stakeholder",
+    }).as("apiCheckStakeholder");
+
+    cy.visit("/controls/business-services");
+
+    // Open modal
+    cy.get("button[aria-label='create-business-service']").click();
+
+    // Verify primary button is disabled
+    cy.get("button[aria-label='submit']").should("be.disabled");
+
+    // Fill form
+    cy.wait("@apiCheckStakeholder");
+    cy.get("input[name='name']").type("my name");
+    cy.get("textarea[name='description']").type("my description");
+
+    cy.get("button.pf-c-select__toggle-button").click();
+    cy.get("button.pf-c-select__menu-item")
+      .eq(0)
+      .click({ waitForAnimations: false });
+
+    cy.get("button[aria-label='submit']").should("not.be.disabled");
+    cy.get("form").submit();
+
+    cy.wait("@apiCheckBusinessService");
+
+    // Verify table
+    cy.get("tbody > tr")
+      .should("have.length", 1)
+      .should("contain", "my name")
+      .and("contain", "my description")
+      .and("contain", "stakeholder1");
   });
 });
