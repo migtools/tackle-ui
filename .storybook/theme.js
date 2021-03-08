d85fb56f32918734cce319794ae64e0d5bb8a383
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2021-03-08 04:30:04 -0800

    Add 'application-inventory' to the Github and development workflow (#14)

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,7 +2,7 @@
 import { create } from '@storybook/theming/create';
 export default create({
   base: 'light',
-  brandTitle: 'Controls',
+  brandTitle: 'Tackle',
   brandUrl: 'https://github.com/konveyor/tackle-ui',
   brandImage: 'https://raw.githubusercontent.com/konveyor/tackle-ui/main/public/logo192.png',
 });
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,21 @@ module.exports = function (app) {
   app.use(
     "/api/controls",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://localhost:8081",
       changeOrigin: true,
       pathRewrite: {
         "^/api/controls": "/controls",
+      },
+    })
+  );
+
+  app.use(
+    "/api/application-inventory",
+    createProxyMiddleware({
+      target: "http://localhost:8082",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/application-inventory": "/application-inventory",
       },
     })
   );
