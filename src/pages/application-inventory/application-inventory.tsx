d5bc4f51ef6728315372072ef7584c4a9e15c627
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-03-08 09:27:13 +0100

    Add app Inventory

--- src/pages/application-inventory/application-inventory.tsx
+++ src/pages/application-inventory/application-inventory.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import {
+  PageSection,
+  PageSectionVariants,
+  Text,
+  TextContent,
+} from "@patternfly/react-core";
 
 export const ApplicationInventory: React.FC = () => {
-  return <>Application inventory: this should be ready soon</>;
+  return (
+    <>
+      <PageSection variant={PageSectionVariants.light}>
+        <TextContent>
+          <Text component="h1">Application inventory</Text>
+        </TextContent>
+        <PageSection>hola</PageSection>
+      </PageSection>
+    </>
+  );
 };
