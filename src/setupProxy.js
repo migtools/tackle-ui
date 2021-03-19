28a74a790338673feded62309ac7013de47afa4b
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-03-19 22:09:29 +0100

    Add ok

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const TACKLE_CONTROLS_HOST =
   process.env.CONTROLS_URL || "http://localhost:8081";
 const TACKLE_APPLICATION_INVENTORY_HOST =
-  process.env.CONTROLS_URL || "http://localhost:8082";
+  process.env.APPLICATION_INVENTORY_URL || "http://localhost:8082";
+
+console.log("Using TACKLE_CONTROLS_HOST=" + TACKLE_CONTROLS_HOST);
+console.log(
+  "Using TACKLE_APPLICATION_INVENTORY_HOST=" + TACKLE_APPLICATION_INVENTORY_HOST
+);
 
 module.exports = function (app) {
   app.use(
