e47fa9a7ac53a85de8511e17dd43ad87eb298c09
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-03-19 21:57:10 +0100

    test yarn

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const path = require("path");
-const app = express(), bodyParser = require("body-parser");
+console.log(process.env.CONTROLS_URL);
+// const express = require("express");
+// const path = require("path");
+// const app = express(), bodyParser = require("body-parser");
 
-const setupProxy = require('./src/setupProxy');
+// const setupProxy = require('./src/setupProxy');
 
-port = 3000;
+// port = 3000;
 
-setupProxy(app);
+// setupProxy(app);
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, "build")));
 
-// Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/build/index.html"));
-});
+// // Handles any requests that don't match the ones above
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/build/index.html"));
+// });
 
-app.listen(port, () => {
-  console.log(`Server listening on the port::${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server listening on the port::${port}`);
+// });
