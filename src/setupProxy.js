2e8c5c3c82acccad08e89008df619682738b70c1
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-04-08 11:34:35 +0200

    Remove local proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,10 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const TACKLE_CONTROLS_HOST =
-  process.env.TACKLE_CONTROLS_HOST || "http://localhost:8081";
-const TACKLE_APPLICATION_INVENTORY_HOST =
-  process.env.TACKLE_APPLICATION_INVENTORY_HOST || "http://localhost:8082";
-const TACKLE_PATHFINDER_HOST =
-  process.env.TACKLE_PATHFINDER_HOST || "http://localhost:8083";
-
 module.exports = function (app) {
   app.use(
     "/api/controls",
     createProxyMiddleware({
-      target: TACKLE_CONTROLS_HOST,
-      secure: false,
+      target: "http://localhost:8081",
       changeOrigin: true,
       pathRewrite: {
         "^/api/controls": "/controls",
@@ -23,8 +15,7 @@ module.exports = function (app) {
   app.use(
     "/api/application-inventory",
     createProxyMiddleware({
-      target: TACKLE_APPLICATION_INVENTORY_HOST,
-      secure: false,
+      target: "http://localhost:8082",
       changeOrigin: true,
       pathRewrite: {
         "^/api/application-inventory": "/application-inventory",
@@ -35,8 +26,7 @@ module.exports = function (app) {
   app.use(
     "/api/pathfinder",
     createProxyMiddleware({
-      target: TACKLE_PATHFINDER_HOST,
-      secure: false,
+      target: "http://localhost:8083",
       changeOrigin: true,
       pathRewrite: {
         "^/api/pathfinder": "/pathfinder",
