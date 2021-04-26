a849f28a91ee71d707c8a39c37813b0673465fa8
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-04-26 12:49:18 +0200

    Add cypress tests

--- cypress/integration/application-inventory/applications/create_new_modal.test.js
+++ cypress/integration/application-inventory/applications/create_new_modal.test.js
@@ -5,8 +5,13 @@ describe("Create new application", () => {
     cy.kcLogout();
     cy.kcLogin("alice").as("tokens");
 
-    // Clean controls
-    cy.get("@tokens").then((tokens) => cy.tackleAppInventoryClean(tokens));
+    // Clean
+    cy.get("@tokens").then((tokens) => {
+      cy.tackleAppInventoryClean(tokens);
+      cy.tackleControlsClean(tokens);
+    });
+
+    const tagTypes = [];
 
     // Create data
     cy.get("@tokens").then((tokens) => {
@@ -17,6 +22,35 @@ describe("Create new application", () => {
           }))
           .forEach((payload) => {
             cy.createBusinessService(payload, tokens);
+          });
+      });
+
+      cy.log("Create tagTypes").then(() => {
+        return [...Array(2)]
+          .map((_, i) => ({
+            name: `tagType-${(i + 10).toString(36)}`,
+          }))
+          .forEach((payload) => {
+            cy.createTagType(payload, tokens).then((data) => {
+              tagTypes.push(data);
+            });
+          });
+      });
+      cy.log("Create tags").then(() => {
+        return [...Array(2)]
+          .map((_, i) => [
+            {
+              name: `tag-a-${(i + 10).toString(36)}`,
+              tagType: tagTypes[i],
+            },
+            {
+              name: `tag-b-${(i + 10).toString(36)}`,
+              tagType: tagTypes[i],
+            },
+          ])
+          .flatMap((a) => a)
+          .forEach((payload) => {
+            cy.createTag(payload, tokens);
           });
       });
     });
@@ -101,5 +135,52 @@ describe("Create new application", () => {
       .eq(0)
       .should("contain", "mybusinessservice")
       .should("contain", "service-a");
+  });
+
+  it("With tags", () => {
+    cy.intercept("GET", new RegExp("/api/controls/tag-type*")).as(
+      "getTagTypesApi"
+    );
+    cy.intercept("GET", new RegExp("/api/controls/tag/*")).as("getTagApi");
+
+    // Open modal
+    cy.get("button[aria-label='create-application']").click();
+    cy.wait("@getTagTypesApi");
+
+    // Verify primary button is disabled
+    cy.get("button[aria-label='submit']").should("be.disabled");
+
+    // Fill form
+    cy.get("input[name='name']").type("myapplication");
+
+    cy.get(".pf-c-form__group-control input.pf-c-select__toggle-typeahead")
+      .eq(1)
+      .type("tag-a-a")
+      .type("{enter}");
+    cy.get(".pf-c-form__group-control input.pf-c-select__toggle-typeahead")
+      .eq(1)
+      .type("tag-b-a")
+      .type("{enter}");
+
+    cy.get("button[aria-label='submit']").should("not.be.disabled");
+    cy.get("form").submit();
+
+    cy.wait("@createApplicationApi");
+    cy.wait("@getApplicationsApi");
+    cy.wait("@getTagApi"); // Fetch first tag
+    cy.wait("@getTagApi"); // Fetch second tag
+
+    // Verify table
+    cy.get(".pf-c-table")
+      .pf4_table_rows()
+      .eq(0)
+      .should("contain", "myapplication")
+      .should("contain", "2"); // 2 tags
+
+    cy.get(".pf-c-table").pf4_table_row_expand(0);
+    cy.get(".pf-c-table > tbody > tr.pf-c-table__expandable-row")
+      .find(".pf-c-label")
+      .should("contain", "tag-a-a")
+      .should("contain", "tag-b-a");
   });
 });
