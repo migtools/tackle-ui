ebb59d8ec0ad8f3e4c53bbc4d717c7b20f740ca4
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-04-27 09:02:25 +0200

    set tags width to 200

--- src/pages/application-inventory/application-list/components/toolbar-search-filter/select-tag-filter/select-tag-filter.tsx
+++ src/pages/application-inventory/application-list/components/toolbar-search-filter/select-tag-filter/select-tag-filter.tsx
@@ -72,14 +72,18 @@ export const SelectTagFilter: React.FC<SelectTagFilterProps> = ({
 
   return (
     <SimpleSelectFetch
+      width={200}
       variant={SelectVariant.checkbox}
       aria-label="tag"
       aria-labelledby="tag"
       placeholderText={t("terms.tag")}
       maxHeight={DEFAULT_SELECT_MAX_HEIGHT}
       value={value
-        .map((f) => (tags || []).find((b) => `${b.id}` === f.key))
-        .map((f) => tagToOption(f!))}
+        .map((f) => {
+          const dbTag = (tags || []).find((b) => `${b.id}` === f.key);
+          return dbTag ? tagToOption(dbTag) : undefined;
+        })
+        .filter((f) => f !== undefined)}
       options={(tags || []).map(tagToOption)}
       onChange={(option) => {
         const optionValue = (option as OptionWithValue<Tag>).value;
