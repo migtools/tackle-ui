13093f0e0949c432beeb1b4b4c5a3cfdceba12ea
mrizzi <mrizzi@redhat.com>
2021-06-18 19:06:01 +0200

    Changed the cypress test for the About modal

--- cypress/integration/template/about.test.js
+++ cypress/integration/template/about.test.js
@@ -10,7 +10,7 @@ context("About modal", () => {
     cy.visit("/");
 
     cy.get("#aboutButton").click();
-    cy.get(".pf-c-about-modal-box").contains("Source code");
+    cy.get(".pf-c-about-modal-box").contains("Tackle is a collection of tools that supports large-scale application modernization and migration projects to Kubernetes.");
     cy.get("button[aria-label='Close Dialog']").click();
   });
 });
