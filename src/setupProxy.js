93a620407167052e010e204966f5bec48a4fc211
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-07-02 11:41:06 +0200

    Add qe tests

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,14 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
+    "/auth",
+    createProxyMiddleware({
+      target: "http://localhost:8180",
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
     "/api/controls",
     createProxyMiddleware({
       target: "http://localhost:8081",
