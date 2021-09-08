53d7c2c8a6ca3687684efe63fafd4bd835f4a1c7
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2021-09-08 15:15:57 +0200

    Add manage dependencies from wizard (#240)

--- src/pages/application-inventory/application-assessment/components/application-assessment-page/application-assessment-page-header.tsx
+++ src/pages/application-inventory/application-assessment/components/application-assessment-page/application-assessment-page-header.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { useTranslation } from "react-i18next";
 
-import { ButtonVariant, Text } from "@patternfly/react-core";
+import { Button, ButtonVariant, Modal, Text } from "@patternfly/react-core";
 
 import { useDispatch } from "react-redux";
 import { confirmDialogActions } from "store/confirmDialog";
 
 import { PageHeader } from "shared/components";
+import { useEntityModal } from "shared/hooks";
+import { ApplicationDependenciesFormContainer } from "shared/containers";
 
 import { Paths } from "Paths";
 import { Application, Assessment } from "api/models";
@@ -34,35 +36,70 @@ export const ApplicationAssessmentPageHeader: React.FC<IApplicationAssessmentPag
     }
   }, [assessment]);
 
+  // Dependencies modal
+  const {
+    isOpen: isDependenciesModalOpen,
+    data: applicationToManageDependencies,
+    update: openDependenciesModal,
+    close: closeDependenciesModal,
+  } = useEntityModal<Application>();
+
   return (
-    <PageHeader
-      title={t("composed.applicationAssessment")}
-      description={<Text component="p">{application?.name}</Text>}
-      breadcrumbs={[
-        {
-          title: t("terms.applications"),
-          path: () => {
-            dispatch(
-              confirmDialogActions.openDialog({
-                title: t("dialog.title.leavePage"),
-                message: t("dialog.message.leavePage"),
-                confirmBtnVariant: ButtonVariant.primary,
-                confirmBtnLabel: t("actions.continue"),
-                cancelBtnLabel: t("actions.cancel"),
-                onConfirm: () => {
-                  dispatch(confirmDialogActions.closeDialog());
-                  history.push(Paths.applicationInventory_applicationList);
-                },
-              })
-            );
+    <>
+      <PageHeader
+        title={t("composed.applicationAssessment")}
+        description={<Text component="p">{application?.name}</Text>}
+        breadcrumbs={[
+          {
+            title: t("terms.applications"),
+            path: () => {
+              dispatch(
+                confirmDialogActions.openDialog({
+                  title: t("dialog.title.leavePage"),
+                  message: t("dialog.message.leavePage"),
+                  confirmBtnVariant: ButtonVariant.primary,
+                  confirmBtnLabel: t("actions.continue"),
+                  cancelBtnLabel: t("actions.cancel"),
+                  onConfirm: () => {
+                    dispatch(confirmDialogActions.closeDialog());
+                    history.push(Paths.applicationInventory_applicationList);
+                  },
+                })
+              );
+            },
           },
-        },
-        {
-          title: t("terms.assessment"),
-          path: Paths.applicationInventory_assessment,
-        },
-      ]}
-      menuActions={[]}
-    />
+          {
+            title: t("terms.assessment"),
+            path: Paths.applicationInventory_assessment,
+          },
+        ]}
+        btnActions={
+          <>
+            {application && (
+              <Button onClick={() => openDependenciesModal(application)}>
+                {t("actions.manageDependencies")}
+              </Button>
+            )}
+          </>
+        }
+        menuActions={[]}
+      />
+
+      <Modal
+        isOpen={isDependenciesModalOpen}
+        variant="medium"
+        title={t("composed.manageDependenciesFor", {
+          what: applicationToManageDependencies?.name,
+        })}
+        onClose={closeDependenciesModal}
+      >
+        {applicationToManageDependencies && (
+          <ApplicationDependenciesFormContainer
+            application={applicationToManageDependencies}
+            onCancel={closeDependenciesModal}
+          />
+        )}
+      </Modal>
+    </>
   );
 };
--- src/shared/components/page-header/page-header.tsx
+++ src/shared/components/page-header/page-header.tsx
@@ -15,6 +15,7 @@ export interface PageHeaderProps {
   title: string;
   description?: React.ReactNode;
   breadcrumbs: { title: string; path: string | (() => void) }[];
+  btnActions?: React.ReactNode;
   menuActions: { label: string; callback: () => void }[];
   navItems?: { title: string; path: string }[];
 }
@@ -23,6 +24,7 @@ export const PageHeader: React.FC<PageHeaderProps> = ({
   title,
   description,
   breadcrumbs,
+  btnActions,
   menuActions,
   navItems,
 }) => {
@@ -39,6 +41,7 @@ export const PageHeader: React.FC<PageHeaderProps> = ({
               {description}
             </TextContent>
           </SplitItem>
+          {btnActions && <SplitItem>{btnActions}</SplitItem>}
           {menuActions.length > 0 && (
             <SplitItem>
               <MenuActions actions={menuActions} />
