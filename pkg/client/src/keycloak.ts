c1d853e347c4f666f2cedd9f35f9e9f227c0b297
Gilles Dubreuil <gilles@redhat.com>
2022-01-26 21:42:28 +0100

    Keycloak use config not json

--- pkg/client/src/keycloak.ts
+++ pkg/client/src/keycloak.ts
@@ -1,7 +1,15 @@
-import Keycloak from "keycloak-js";
+import Keycloak, { KeycloakConfig } from "keycloak-js";
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
-const keycloak = Keycloak(process.env.PUBLIC_URL + "/keycloak.json");
+// const keycloak = Keycloak(process.env.PUBLIC_URL + "/keycloak.json");
+
+const config: KeycloakConfig = {
+  url: "/auth",
+  realm: "tackle",
+  clientId: "tackle-ui",
+};
+
+const keycloak = Keycloak(config);
 
 export default keycloak;
