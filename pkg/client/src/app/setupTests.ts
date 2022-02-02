840bbd0e1a71a1ab38ed416fbf05b06ca083409b
Gilles Dubreuil <gilles@redhat.com>
2022-02-02 18:58:36 +0100

    Revert "ts loader instead of babel"
    This reverts commit fa1ba7e0cd62fb42769050973f7dd48bc3a6cabe.

--- pkg/client/src/app/setupTests.ts
+++ pkg/client/src/app/setupTests.ts
@@ -7,4 +7,5 @@ import "@testing-library/jest-dom";
 import "jest-enzyme";
 import { configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
 configure({ adapter: new Adapter() });
--- pkg/client/src/index.tsx
+++ pkg/client/src/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "@app/App";
-import reportWebVitals from "@app/reportWebVitals";
+import App from "./app/App";
+import reportWebVitals from "./app/reportWebVitals";
 
 import { Provider } from "react-redux";
-import configureStore from "@app/store";
+import configureStore from "./app/store";
 
 import { initApi, initInterceptors } from "@app/axios-config";
 
 import { ReactKeycloakProvider } from "@react-keycloak/web";
-import keycloak from "@app/keycloak";
+import keycloak from "./app/keycloak";
 
-import i18n from "@app/i18n";
-import { NinjaErrorBoundary } from "@app/ninja-error-boundary";
+import i18n from "./app/i18n";
+import { NinjaErrorBoundary } from "./app/ninja-error-boundary";
 
 import "./index.css";
 
@@ -20,41 +20,43 @@ initApi();
 i18n.init();
 
 ReactDOM.render(
-  <ReactKeycloakProvider
-    authClient={keycloak}
-    initOptions={{ onLoad: "login-required" }}
-    LoadingComponent={<span>Loading...</span>}
-    isLoadingCheck={(keycloak) => {
-      if (keycloak.authenticated) {
-        initInterceptors(() => {
-          return new Promise<string>((resolve, reject) => {
-            if (keycloak.token) {
-              keycloak
-                .updateToken(5)
-                .then(() => resolve(keycloak.token!))
-                .catch(() => reject("Failed to refresh token"));
-            } else {
-              keycloak.login();
-              reject("Not logged in");
-            }
+  <React.StrictMode>
+    <ReactKeycloakProvider
+      authClient={keycloak}
+      initOptions={{ onLoad: "login-required" }}
+      LoadingComponent={<span>Loading...</span>}
+      isLoadingCheck={(keycloak) => {
+        if (keycloak.authenticated) {
+          initInterceptors(() => {
+            return new Promise<string>((resolve, reject) => {
+              if (keycloak.token) {
+                keycloak
+                  .updateToken(5)
+                  .then(() => resolve(keycloak.token!))
+                  .catch(() => reject("Failed to refresh token"));
+              } else {
+                keycloak.login();
+                reject("Not logged in");
+              }
+            });
           });
-        });
 
-        const kcLocale = (keycloak.tokenParsed as any)["locale"];
-        if (kcLocale) {
-          i18n.changeLanguage(kcLocale);
+          const kcLocale = (keycloak.tokenParsed as any)["locale"];
+          if (kcLocale) {
+            i18n.changeLanguage(kcLocale);
+          }
         }
-      }
 
-      return !keycloak.authenticated;
-    }}
-  >
-    <Provider store={configureStore()}>
-      <NinjaErrorBoundary>
-        <App />
-      </NinjaErrorBoundary>
-    </Provider>
-  </ReactKeycloakProvider>,
+        return !keycloak.authenticated;
+      }}
+    >
+      <Provider store={configureStore()}>
+        <NinjaErrorBoundary>
+          <App />
+        </NinjaErrorBoundary>
+      </Provider>
+    </ReactKeycloakProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
