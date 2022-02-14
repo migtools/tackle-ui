3cac5861319feab617d8350b1b565b135fedb1f7
Ian Bolton <ibolton@redhat.com>
2022-02-14 16:43:50 -0500

    Fix production build

--- pkg/client/config/stylePaths.js
+++ pkg/client/config/stylePaths.js
@@ -2,7 +2,7 @@
 const path = require("path");
 module.exports = {
   stylePaths: [
-    path.resolve(__dirname, "..//src"),
+    path.resolve(__dirname, "../src"),
     path.resolve(__dirname, "../node_modules/patternfly"),
     path.resolve(__dirname, "../node_modules/@patternfly/patternfly"),
     path.resolve(__dirname, "../node_modules/@patternfly/react-styles/css"),
